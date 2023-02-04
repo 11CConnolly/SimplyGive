28d95c920ddaca4392023ec17dcc1ffc87270d91
cconnolly <cconnolly@criticalsoftware.com>
2023-02-04 14:14:28 +0000

    BE Changes

--- server/index.js
+++ server/index.js
@@ -25,87 +25,9 @@ if (process.env.NODE_ENV !== "test") {
     .then(() => console.log("Connected to Database"));
 }
 
-const save = async (params) => {
-  const charity = new Charity({ ...params });
-  await charity.save();
-};
-
 // Define our API logic
-// TODO refactor to routes api
-app.post("/api/charity", async (req, res) => {
-  try {
-    await save(req.body);
-    res.status(201).json({
-      status: "Success",
-      description: "charity created",
-    });
-  } catch (err) {
-    if (err.name === "ValidationError") {
-      res.status(400).json({
-        status: "Failed",
-        description: "invalid input, object invalid",
-      });
-    } else {
-      res.status(500).json({
-        status: "Failed",
-        message: err,
-      });
-    }
-  }
-});
-
-app.get("/api/charity", async (req, res) => {
-  const charities = await Charity.find({});
-  try {
-    res.status(200).json({
-      status: "Success",
-      description: "all charities in database",
-      charities,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-});
-
-app.post("/api/charity/findByCategory", async (req, res) => {
-  const charities = await Charity.find({
-    categories: { $in: req.body.categories },
-  });
-  try {
-    res.status(200).json({
-      status: "Success",
-      description: "Successful operation",
-      charities,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-});
-
-app.post("/api/charity/findSingleByCategory", async (req, res) => {
-  const charities = await Charity.find({
-    categories: { $in: req.body.categories },
-  });
-  const charity = charities[Math.floor(Math.random() * charities.length)];
-  try {
-    res.status(200).json({
-      status: "Success",
-      description: "Successful operation",
-      charity,
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "Failed",
-      message: err,
-    });
-  }
-});
+const charityRoutes = require("./routes/charity");
+app.use("/api/charity", charityRoutes);
 
 const PORT = process.env.PORT || 8080;
 

0ecaec3bdb87fa823b05b229d85151f8140bcacd
11CConnolly <callumc11@gmail.com>
2023-04-21 19:21:09 +0100

    Adding UI layout to Landing page

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -10,4 +10,10 @@ const categories = {
 const formatAsGBP = (val) => "£" + val;
 const parseAsGBP = (val) => val.replace(/^£/, "");
 
-module.exports = { categories, formatAsGBP, parseAsGBP };
+const zIndexLevels = {
+  LEVEL_MED: 1,
+  LEVEL_HIGH: 10,
+  LEVEL_ULTRA_HIGH: 100,
+};
+
+module.exports = { categories, formatAsGBP, parseAsGBP, zIndexLevels };

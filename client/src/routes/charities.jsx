763f84a98f711601e25e2b4f71874486c1a9ebed
11CConnolly <callumc11@gmail.com>
2023-04-23 22:28:30 +0100

    removing unused vars

--- client/src/routes/charities.jsx
+++ client/src/routes/charities.jsx
@@ -3,8 +3,6 @@ import useDataApi from "../hooks/useDataApi";
 
 const Charities = () => {
   // The data I expect returned from this is a list of users
-  const [dataState] = useDataApi("/api/user");
-
   return <></>;
 };
 
